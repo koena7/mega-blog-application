575bdd8190bed4c23fd3a564551de945659a7d70
koena7 <koena2204@gmail.com>
2026-01-31 18:04:16 +0530

    done with login page

--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -1,7 +1,36 @@
 import Logo from "../footer/Logo";
-
+import { useForm } from "react-hook-form";
+import Input from "../utility/Input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login, getCurrentUser } from "../../state-management/authSlice";
+import authService from "../../appwrite-services/AuthenticationService";
+import { useNavigate } from "react-router-dom";
 
 function Login(){
+
+    const {register, handleSubmit} = useForm();
+    const [error, setError] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async(formData) => {
+        setError('');
+        try {
+            const session = await authService.logIn(formData.email, formData.password);
+            if(session){
+                const userData = await getCurrentUser();
+                if(userData){
+                    dispatch(login(userData))
+                    navigate('/')
+                }
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    } 
+
     return(
         <div className="w-full h-">
             {/* Logo */}
@@ -11,17 +40,35 @@ function Login(){
 
             {/* Errors */}
             <div>
-
+                {error && <div className="text-red-500">{error}</div>}
             </div>
 
             {/* form */}
-            <div>
-                
-            </div>
+            <form onSubmit={handleSubmit(handleLogin)}>
+                <div>
+                    <Input
+                        label='Email ID'
+                        placeholder='janedoe@gmail.com'
+                        {...register('email',{
+                            required: true,
+                            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+                        })}
+                    />
+                    <Input
+                        label='Password'
+                        type='password'
+                        {...register('password',{
+                            required: true
+                        })}
+                    />
+                    <Button type='submit'>Submit</Button>
+                </div>
+            </form>
 
             {/* not a user?Sign up. */}
             <div>
-
+                Don't have an account?
+                <Link to='signup'>SignUp</Link>
             </div>
         </div>
     )
